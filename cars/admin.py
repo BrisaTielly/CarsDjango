5ca07177d226423bc0fbf5aa5c697b0f4691e864
Brisa Silva <brisatielly12@gmail.com>
2025-03-04 18:31:36 -0300

    refactor: Adiciona no modelo Car o campo de placa e de imagem

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from cars.models import Car, Brand
 
@@ -8,8 +9,15 @@ class BrandAdmin(admin.ModelAdmin):
     search_fields = ('name', )
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    #Campos que vão aparecer para o admin
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value') 
+
+    #Campos que vão servir de busca
     search_fields = ('model', 'brand', )
 
 admin.site.register(Car, CarAdmin)
 admin.site.register(Brand, BrandAdmin)
+
+
+
+
--- cars/models.py
+++ cars/models.py
@@ -12,11 +12,13 @@ class Brand(models.Model):
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=100)
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT) #Modo de deletar protegido
     factory_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-
+    photo = models.ImageField(upload_to='/cars', blank=True, null=True)
+    
     def __str__(self):
         return self.model
 
